cce8e5bb9a14cd565aa1283190a1aba9e0ea6d45
sbhalla98 <sonalibhalla98@gmail.com>
2019-08-11 21:00:44 +0530

    css styled

--- cryptowiki/src/App.js
+++ cryptowiki/src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
 
   constructor(){
     super();
+    //state object to store necessary information needed
     this.state = {
       hideloader : false,
       coinlist : [],
@@ -25,11 +26,12 @@ class App extends Component {
       details : null,
       homepage:null,
       symbol:null
-
     }
     this.infoHandler=this.infoHandler.bind(this);
   }
 
+// method call for displaying clientInformation
+// calling specifying api depending upon id
 async infoHandler(x){
   var id = x.toLowerCase();
   const coinsList = this.state.coinlist;
@@ -43,6 +45,10 @@ async infoHandler(x){
   const data = await res.json();
   this.setState({searchlabel : true,title: data.name,symbol: data.symbol,url : data.image.large,container:true,countryOrigin: data.country_origin,score: data.developer_score,communityScore: data.community_score,liquidityScore: data.liquidity_score,publicInterestScore: data.public_interest_score,cgRank: data.coingecko_rank,cgScore:data.coingecko_score,capRank: data.market_data.market_cap_rank, date : data.genesis_date, details: data.description.en,homepage: data.links.subreddit_url});
 }
+
+
+//method call after render method
+//calling api to load the list of coins
 async componentDidMount(){
   const res = await fetch("https://api.coingecko.com/api/v3/coins/list");
   const data = await res.json();
@@ -50,6 +56,7 @@ async componentDidMount(){
 }
 
 render(){
+  // storing information in variables to use it in webpage
   const style = this.state.searchlabel ? {display : 'none'}:{};
   const style1 = this.state.container ? {}:{display : 'none'};
   var title = this.state.title;
@@ -68,15 +75,20 @@ render(){
   var symbol = this.state.symbol;
     return (
     <div className="main" >
-       
-       <h1 className="header">Crypto Wiki</h1>
-        {this.state.hideloader ? <div><div><Form  infoHandler={this.infoHandler}/><div className="label" style={style}><h4>Search For a Coin</h4></div></div></div> : <div className="loader">Loading The Coin List</div>   }
+       {/* page title */}
+      <h1 className="header">Crypto Wiki</h1>
+      {/* setting search bar and loading meassage */}
+      {this.state.hideloader ? <div><div><Form  infoHandler={this.infoHandler}/><div className="label" style={style}><h4>Search For a Coin</h4></div></div></div> : <div className="loader">Loading The Coin List</div>   }
+      
+      {/* panel that contain search currency information */}
       <div className="component-container" style={style1}>
-    <a href={homepageurl}><div className="panelTitle" >{title} - {symbol}</div></a>
-        <div className="panelLeft">
-          <div className="coinImage">
-            <img src ={url} style={{width : '100%'}} alt={title}/>
-          </div>
+      <a href={homepageurl}><div className="panelTitle" >{title} - {symbol}</div></a>
+
+      {/* first division of panel */}
+      <div className="panelLeft">
+      <div className="coinImage">
+      <img src ={url} style={{width : '100%'}} alt={title}/>
+      </div>
           <div className="coinDetail">
             <div className="deatailabel">Country of Origin -</div>
             <div className="info">{origin}</div>
@@ -95,12 +107,12 @@ render(){
             <div className="info">{liquidityScore}</div>
             <div className="deatailabel">Public Interest Score -</div>
             <div className="info">{publicInterestScore}</div>
-          </div>
         </div>
+        </div>
+         {/* second division of panel */}
         <div className="panelRight">{details}</div>
       </div>
-    
-    </div>
+     </div>
    );
  }
  }
